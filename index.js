4d5528a6bc9bcdcf7895fa5fef6b7714a9269480
Alberto Dallaporta <alberto@novalab.io>
2017-05-24 17:41:16 +0200

    Improved tx watcher by using Promises - Bump contracts abis and addresses

--- index.js
+++ index.js
@@ -12,15 +12,32 @@ const token_address = require('./contracts/Token.address'),
       registry_address = require('./contracts/Registry.address'),
       delivery_service_address = require('./contracts/DeliveryService.address')
 
-module.exports = function(provider, verbose) {
+let intervalIDs = {}
 
-  var tasks = {}
-  var task_timer = setInterval(function() {
-    var tx_hashes = Object.keys(tasks)
-    for (var i = tx_hashes.length - 1; i >= 0; i--) {
-      tasks[tx_hashes[i]].task()
-    }
-  }, 5 * 1000)
+function watchExecution(txhash, web3) {
+  return new Promise(function(resolve, reject) {
+    let ttl = 60 // 60 tries, once every 5 seconds === 5 minutes
+    intervalIDs[txhash] = setInterval(function() {
+      web3.eth.getTransactionReceipt(txhash, function(error, receipt) {
+        if (error || ttl < 0) {
+          reject(error || new Error('ran out of retries waiting for tx confirmation'))
+          clearInterval(intervalIDs[txhash])
+          return
+        }
+        
+        if (receipt && Array.isArray(receipt.logs) && receipt.logs.length) {
+          resolve(`0x${receipt.logs[0].data.slice(26)}`)
+          clearInterval(intervalIDs[txhash])
+          return
+        }
+        
+        ttl--
+      })
+    }, 5 * 1000)
+  })
+}
+
+module.exports = function(provider) {
 
   const web3 = typeof provider === 'string' ? new Web3(new Web3.providers.HttpProvider(provider)) : provider
 
@@ -32,131 +49,121 @@ module.exports = function(provider, verbose) {
         Registry = registryInterface.at(registry_address),
         DeliveryService = deliveryServiceInterface.at(delivery_service_address)
 
-  function new_task(hash, callback) {
-    tasks[hash] = {
-      ttl: 100,
-      callback: callback
-    }
-    tasks[hash].task = function(tx) {
-      web3.eth.getTransactionReceipt(tx, function(err, receipt) {
-        var removeflag = false
-        if (err || this.ttl < 0) {
-          removeflag = true
-          this.callback(err || new Error('ran out of retries waiting for tx confirmation'))
-        }
-        if (!removeflag &&
-            receipt &&
-            ((web3.eth.blockNumber - receipt.blockNumber) > 0) &&
-            Array.isArray(receipt.logs) &&
-            receipt.logs.length
-        ) {
-          removeflag = true
-          this.callback(null, `0x${receipt.logs[0].data.slice(26)}`)
-        }
-        if (removeflag) delete tasks[tx]
-        else this.ttl -= 1
-      }.bind(this))
-    }.bind(tasks[hash], hash)
-  }
-
   return {
 
-    record: function(vchash, timestamp, centreDID, unitCode, senderPrivateKey, callback) {
-      const calldata = Registry.record.getData(vchash, timestamp, unitCode, centreDID)
-      
-      const signer_buf = Buffer.from(senderPrivateKey, 'hex'),
-            signer_addr = `0x${ut.privateToAddress(signer_buf).toString('hex')}`
-
-      web3.eth.getTransactionCount(signer_addr, (err, nonce) => {
+    record: function(vchash, date, centreDID, unitCode, senderPrivateKey) {
+      return new Promise(function (parentResolve, parentReject) {
+        const calldata = Registry.record.getData(vchash, date, unitCode, centreDID)
         
-        if (err)
-          return callback(err)
+        const signer_buf = Buffer.from(senderPrivateKey, 'hex'),
+              signer_addr = `0x${ut.privateToAddress(signer_buf).toString('hex')}`
 
-        const transaction = new Tx({
-          to: registry_address,
-          gasLimit: 500000,
-          gasPrice: +web3.toWei(20, 'gwei'),
-          nonce: nonce,
-          data: calldata,
-        })
-
-        transaction.sign(signer_buf)
+        web3.eth.getTransactionCount(signer_addr, (err, nonce) => {
         
-        const raw = `0x${transaction.serialize().toString('hex')}`
+          if (err) {
+            parentReject(err)
+            return
+          }
 
-        return web3.eth.sendRawTransaction(raw, function(error, txhash) {
-          if (error)
-            return callback(error)
+          const transaction = new Tx({
+            to: registry_address,
+            gasLimit: 500000,
+            gasPrice: +web3.toWei(20, 'gwei'),
+            nonce,
+            data: calldata,
+          })
+
+          transaction.sign(signer_buf)
           
-          return verbose ? new_task(txhash, callback) : callback(error, txhash)
+          const raw = `0x${transaction.serialize().toString('hex')}`
+
+          web3.eth.sendRawTransaction(raw, function(error, txhash) {
+            if (error) {
+              parentReject(error)
+              return
+            }
+            
+            watchExecution(txhash, web3).then(parentResolve).catch(parentReject)
+          })
         })
       })
     },
 
-    execute: function(to, value, vchash, senderPrivateKey, callback) {
-      const calldata = DeliveryService.execute.getData(to, value, vchash)
-      
-      const signer_buf = Buffer.from(senderPrivateKey, 'hex'),
-            signer_addr = `0x${ut.privateToAddress(signer_buf).toString('hex')}`
+    execute: function(to, value, vchash, senderPrivateKey) {
+      return new Promise(function (parentResolve, parentReject) {
+        const calldata = DeliveryService.execute.getData(to, value, vchash)
 
-      web3.eth.getTransactionCount(signer_addr, (err, nonce) => {
+        const signer_buf = Buffer.from(senderPrivateKey, 'hex'),
+              signer_addr = `0x${ut.privateToAddress(signer_buf).toString('hex')}`
         
-        if (err)
-          return callback(err)
-
-        const transaction = new Tx({
-          to: delivery_service_address,
-          gasLimit: 4000000,
-          gasPrice: +web3.toWei(20, 'gwei'),
-          nonce: nonce,
-          data: calldata,
-        })
-
-        transaction.sign(signer_buf)
+        web3.eth.getTransactionCount(signer_addr, (err, nonce) => {
         
-        const raw = `0x${transaction.serialize().toString('hex')}`
+          if (err) {
+            parentReject(err)
+            return
+          }
 
-        return web3.eth.sendRawTransaction(raw, function(error, txhash) {
-          if (error)
-            return callback(error)
+          const transaction = new Tx({
+            to: delivery_service_address,
+            gasLimit: 4000000,
+            gasPrice: +web3.toWei(20, 'gwei'),
+            nonce,
+            data: calldata,
+          })
+
+          transaction.sign(signer_buf)
           
-          return verbose ? new_task(txhash, callback) : callback(error, txhash)
+          const raw = `0x${transaction.serialize().toString('hex')}`
+
+          return web3.eth.sendRawTransaction(raw, function(error, txhash) {
+            if (error) {
+              parentReject(error)
+              return
+            }
+
+            watchExecution(txhash, web3).then(parentResolve).catch(parentReject)
+          })
         })
       })
     },
 
-    confirm: function(vchash, senderPrivateKey, callback) {
-      const calldata = DeliveryService.confirm.getData(vchash)
+    confirm: function(vchash, senderPrivateKey) {
+      return new Promise(function(parentResolve, parentReject) {
+        const calldata = DeliveryService.confirm.getData(vchash)
       
-      const signer_buf = Buffer.from(senderPrivateKey, 'hex'),
-            signer_addr = `0x${ut.privateToAddress(signer_buf).toString('hex')}`
-
-      web3.eth.getTransactionCount(signer_addr, (err, nonce) => {
+        const signer_buf = Buffer.from(senderPrivateKey, 'hex'),
+              signer_addr = `0x${ut.privateToAddress(signer_buf).toString('hex')}`
         
-        if (err)
-          return callback(err)
-
-        const transaction = new Tx({
-          to: delivery_service_address,
-          gasLimit: 4000000,
-          gasPrice: +web3.toWei(20, 'gwei'),
-          nonce: nonce,
-          data: calldata,
-        })
-
-        transaction.sign(signer_buf)
+        web3.eth.getTransactionCount(signer_addr, (err, nonce) => {
         
-        const raw = `0x${transaction.serialize().toString('hex')}`
+          if (err) {
+            parentReject(err)
+            return
+          }
 
-        return web3.eth.sendRawTransaction(raw, function(error, txhash) {
-          if (error)
-            return callback(error)
+          const transaction = new Tx({
+            to: delivery_service_address,
+            gasLimit: 4000000,
+            gasPrice: +web3.toWei(20, 'gwei'),
+            nonce,
+            data: calldata,
+          })
+
+          transaction.sign(signer_buf)
           
-          return verbose ? new_task(txhash, callback) : callback(error, txhash)
+          const raw = `0x${transaction.serialize().toString('hex')}`
+
+          return web3.eth.sendRawTransaction(raw, function(error, txhash) {
+            if (error) {
+              parentReject(error)
+              return
+            }
+
+            watchExecution(txhash, web3).then(parentResolve).catch(parentReject)
+          })
         })
       })
     }
 
   }
 }
-
